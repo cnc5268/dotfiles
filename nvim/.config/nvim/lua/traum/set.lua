390991d3783d035f19f5b27a6e76156aa4f9f7f8
cnc5268 <favor.osemeilu@tuhh.de>
2025-01-31 23:36:06 +0100

    Mass dump

--- nvim/.config/nvim/lua/traum/set.lua
+++ nvim/.config/nvim/lua/traum/set.lua
@@ -23,10 +23,11 @@ vim.opt.incsearch = true
 
 vim.opt.termguicolors = true
 
+vim.wo.wrap = true
 vim.opt.scrolloff = 8
 vim.opt.signcolumn = "yes"
 vim.opt.isfname:append("@-@")
 
 vim.opt.updatetime = 50
 
-vim.opt.colorcolumn = "90"
+--vim.opt.colorcolumn = "90"
